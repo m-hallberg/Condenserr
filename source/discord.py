7ca9a176a10a9717daa275b7ca6b5ffd8ca3c1ef
Michael Hallberg <143094009+m-hallberg@users.noreply.github.com>
2025-04-02 06:59:34 -0400

    Refocused organization

--- source/discord.py
+++ source/discord.py
@@ -6,10 +6,12 @@ from .models import Item, Episode
 discord = Blueprint('discord', __name__)
 
 def send_webhook():
+    count = 0
     webhook = DiscordWebhook(url=os.getenv('WEBHOOK_URL'))
     embed = DiscordEmbed(title='New Media Added!', description="New media has been added to the server! Check it out below to see what's new! Enjoy!")
     webhook.add_embed(embed)
     for item in Item.query.all():
+        count += 1
         embed = DiscordEmbed(title = item.name, description = item.description, url = item.local_url)
         embed.set_thumbnail(item.img_url)
         if item.type == 'Series':
@@ -22,6 +24,9 @@ def send_webhook():
                 episode_list = ', '.join(map(str, all_episodes[season]))
                 embed.add_embed_field(name=f"Season {season}", value=episode_list)
         webhook.add_embed(embed)
+        if count == 9:
+            webhook.execute()
+            webhook = DiscordWebhook(url=os.getenv('WEBHOOK_URL'))
     webhook.execute()
 
 @discord.route('/send', methods=['GET'])
